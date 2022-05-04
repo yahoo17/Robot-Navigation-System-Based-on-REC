e4bccef7c0f4fd4ed5491436db5908a62db25455
yahoo17 <yanhaoo2000@gmail.com>
2022-05-04 14:31:27 +0800

    rpc_cli and rpc sever

---
+++ mm_rpc_server.py
@@ -0,0 +1,20 @@
+from agileutil.rpc.server import TcpRpcServer, rpc
+import asyncio
+
+@rpc
+class TestService:
+
+    def hello(self, name):
+        return "Hello, {}!".format(name)
+
+    async def add(self, a, b, c):
+        asyncio.sleep(1)
+        return a + b + c
+
+@rpc
+def hello(name):
+    return "Hello, {}!".format(name)
+
+server = TcpRpcServer('0.0.0.0', 9988)
+server.serve()
+
