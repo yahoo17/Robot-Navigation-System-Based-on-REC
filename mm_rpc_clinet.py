5b7a53ef0d19cb28603face51c71b0d9a85cd386
yahoo17 <yahoo2000@126.com>
2022-05-04 14:45:07 +0800

    1

--- mm_rpc_clinet.py
+++ mm_rpc_clinet.py
@@ -1,7 +1,7 @@
 from agileutil.rpc.client import TcpRpcClient
 
-cli = TcpRpcClient('127.0.0.1', 9988, timeout = 2)
-
+cli = TcpRpcClient('127.0.0.1', 2061, timeout = 2)
+#cli = TcpRpcClient('202.38.247.165', 2061, timeout=2)
 
 resp = cli.call('TestService.add', a=1, b=2, c=3)
 print(resp)
