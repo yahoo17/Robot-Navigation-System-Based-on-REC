07b8ebadab76c8215154be3f843fb99daf5b4b56
yahoo17 <yanhaoo2000@gmail.com>
2022-05-06 16:10:30 +0800

    change client main

--- mm_rpc_client.py
+++ mm_rpc_client.py
@@ -7,11 +7,6 @@ def decode_picture(base64_data,file_name):
         jiema = base64.b64decode(base64_data)  # 解码
         file.write(jiema)  # 将解码得到的数据写入到图片中
 
-cli = TcpRpcClient('127.0.0.1', 2061, timeout = 2)
-
-
-resp = cli.call('TestService.add', a=1, b=2, c=3)
-print(resp)
 
 # resp = cli.call('TestService.hello', name = 'xiaoming')
 # print(resp)
@@ -23,3 +18,10 @@ def get_image(instruction_id):
         decode_picture(file,str(index))
     return len(resp), resp
 
+
+if __name__ == "__main__":
+
+    cli = TcpRpcClient('127.0.0.1', 2061, timeout = 2)
+
+    resp = cli.call('TestService.add', a=1, b=2, c=3)
+    print(resp)
